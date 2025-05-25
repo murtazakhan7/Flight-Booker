6446a5c26db144567f54f7da1e52967b9237ae10
Muhammad Murtaza <126237204+murtazakhan7@users.noreply.github.com>
2025-05-25 10:07:01 -0700

    Update models.py

--- project2/flights/flight/models.py
+++ project2/flights/flight/models.py
@@ -1,29 +1,24 @@
 from django.db import models
 
 # Create your models here.
-
 class Airport(models.Model):
-    code=models.CharField(max_length=5)
-    city=models.CharField(max_length=100)
+    code = models.CharField(max_length=5)
+    city = models.CharField(max_length=100)
     
     def __str__(self):
-        return f"{self.code, self.city}"
+        return f"{self.code} - {self.city}"
     
-
-
 class Flights(models.Model):
-    
-    origin=models.ForeignKey(Airport,on_delete=models.CASCADE,related_name='origin')
-    destination=models.ForeignKey(Airport,on_delete=models.CASCADE, related_name='destionation')
-    duration=models.IntegerField()
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='departing_flights')
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='arriving_flights')
+    duration = models.IntegerField()
     
     def __str__(self):
-        return f"{self.id  , self.origin , self.destination, self.duration}"
+        return f"Flight {self.id}: {self.origin} to {self.destination} ({self.duration} min)"
 
 class Passenger(models.Model):
-    name=models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
     flights = models.ManyToManyField(Flights, blank=True, related_name="passengers")
-    
     
     def __str__(self):
         return self.name
